210d9614448fa098fa8e2976094f6724053701f4
Buksbuny <so18-45@ukr.net>
2019-01-01 20:25:26 +0000

    json

---
+++ js/index.js
@@ -0,0 +1,25 @@
+/*--------------Carousel---------*/
+*('.carousel').carousel({
+  interval: 4000
+})
+/*----------------Json-----------*/
+document.getElementById('page-loaded').innerHTML=
+(new Date()).toLocaleTimeString();
+
+document.querySelector('button')
+    .addEventListener('click', getData);
+    
+    function getData(e) {
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                const client = JSON.parse(xhr.responseText);
+                document.getElementById('client-name').innerHTML = client.name;
+                document.getElementById('client-address').innerHTML = client.address;
+                document.getElementById('client-age').innerHTML = client.age;
+                
+            }
+        }
+        xhr.open('GET', 'client.json', true);
+        xhr.send();
+    }
